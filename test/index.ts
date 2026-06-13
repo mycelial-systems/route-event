53ce047cbebdba5a0c33a12a2f4d079bede7a90b
nichoth <nichoth@nichoth.com>
2026-06-13 00:47:35 -0700

    fix anchor link bug

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,7 @@
 import { test } from '@substrate-system/tapzero'
 import { click, dom } from '@substrate-system/dom'
 import Route from '../src/index.js'
+import { singlePage } from '../src/single-page.js'
 
 let unlisten
 test('route event, with root call', t => {
@@ -39,4 +40,34 @@ test('Use a function to check links', t => {
 
     click('#def')
     click('#abc')
+})
+
+test('handleAnchor:false routes a hash URL to the base path', t => {
+    t.plan(1)
+    const onRoute = Route({ handleAnchor: false, init: false })
+    const stop = onRoute(newPath => {
+        t.equal(newPath, '/',
+            'should strip the hash and route to the base path')
+    })
+    onRoute.setRoute('/#pricing')
+    stop()
+})
+
+test('singlePage honors a boolean handleAnchor:false', t => {
+    t.plan(1)
+    const setRoute = singlePage(newPath => {
+        t.equal(newPath, '/', 'should route the hash URL to the base path')
+    }, { handleAnchor: false, init: false })
+    setRoute('/#pricing')
+})
+
+test('default handleAnchor passes the hash through to the callback', t => {
+    t.plan(1)
+    const onRoute = Route({ init: false })
+    const stop = onRoute(newPath => {
+        t.equal(newPath, '/#pricing',
+            'should pass the full hash path through by default')
+    })
+    onRoute.setRoute('/#pricing')
+    stop()
 })
